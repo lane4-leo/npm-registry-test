8e449f133822893b94754290fdd9638987da5f6f
lane4-leo <leo@lane4.ai>
2023-05-11 11:27:23 +0900

    chore: 마지막 테스트

--- src/print.test.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PrintTest2 = void 0;
-function PrintTest2() {
-    console.log('print test 입니다.');
-    return 'print test';
-}
-exports.PrintTest2 = PrintTest2;
-//# sourceMappingURL=print.test.js.map
--- src/print.test.ts
+++
@@ -1,4 +0,0 @@
-export function PrintTest2() {
-    console.log('print test 입니다.')
-    return 'print test';
-}
