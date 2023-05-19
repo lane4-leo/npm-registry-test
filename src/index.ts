9f3d8abca8dd65179bfa5df7e077884c2f639e32
lane4-leo <leo@lane4.ai>
2023-05-19 13:55:42 +0900

    butils export 추가

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import {PrintModule} from '../src/print/print.module'
 import {PrintService} from '../src/print/print.service'
 import {AUtils} from './a.utils'
-export {PrintModule, PrintService, AUtils}
+import {BUtils} from './b.utils'
+export {PrintModule, PrintService, AUtils, BUtils}
