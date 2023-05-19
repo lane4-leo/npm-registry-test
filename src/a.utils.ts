affaf935c42639396032f7eafbc6a23ee0b1d7fe
lane4-leo <leo@lane4.ai>
2023-05-19 14:19:03 +0900

    test

--- src/a.utils.ts
+++ src/a.utils.ts
@@ -1,4 +1,12 @@
-export const AUtils = () => {
-        console.log('A Print Test 입니.')
-        return 'A Print Test';
+export class AUtils {
+        public static async APrint() {
+                console.log('A Print Test 입니.')
+                return 'A Print Test';
+
+        }
 }
+
+// export const AUtils = () => {
+//         console.log('A Print Test 입니.')
+//         return 'A Print Test';
+// }
