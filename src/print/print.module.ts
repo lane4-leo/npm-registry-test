c9fec2a125a8744e4eb7bda16d7ab56aa1d6d7c0
lane4-leo <leo@lane4.ai>
2023-05-19 13:58:29 +0900

    finish test

--- src/print/print.module.ts
+++ src/print/print.module.ts
@@ -1,10 +1,9 @@
 import {Module} from '@nestjs/common';
 import {PrintService} from './print.service';
 import {BUtils} from "../b.utils";
-import {AUtils} from "../a.utils";
 
 @Module({
-    providers: [PrintService,AUtils,BUtils],
+    providers: [PrintService,BUtils],
     exports: [PrintService,BUtils]
 })
 export class PrintModule {
